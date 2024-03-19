14c4dd1a956b971e2a7a9f69c4842eb522a4ae62
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-03-19 15:40:45 -0500

    Update mamifero.py

--- zooAnimales/mamifero.py
+++ zooAnimales/mamifero.py
@@ -1,15 +1,37 @@
-from zooAnimales.animal import Animal
+from animal import Animal
 
 class Mamifero(Animal):
-    self_listado = []
-
-    def __init__(self, nombre, edad, habitat, genero):
+    _listado = []
+    def __init__(self, nombre, edad, habitat, genero, pelaje, patas):
         super().__init__(nombre, edad, habitat, genero)
+        self._pelaje = pelaje
+        self._patas = patas
+        self.caballos=0
+        self.leones = 0
+        Mamifero._listado.append(self)
 
+    #get
+    def getPelaje(self):
+        return self._pelaje
+    def getPatas(self):
+        return self._patas
+    #set
+    def setPelaje(self, pelaje):
+        self._pelaje = pelaje
+    def setPatas(self, patas):
+        self._patas= patas
+    def isPelaje(self):
+        return self._pelaje
+    
+    @classmethod
+    def crearCaballo(cls, nombre,edad, genero, pelaje = True, patas = 4, habitat = "pradera"):
 
+        return cls(nombre, edad, genero, pelaje, patas, habitat)
 
-animal = Animal("liz", 5, "medellin", "f")
+    @classmethod
+    def crearLeon(cls, nombre, edad, genero, pelaje = True, patas = 4, habitat = "selva"):
+        return cls(nombre, edad, genero, pelaje, patas, habitat)
+    
+    def cantidadMamiferos(self):
+        return len(Mamifero._listado)       
 
-mamifero = Mamifero("perro", 1,1,1)
-
-print(mamifero._nombre)
